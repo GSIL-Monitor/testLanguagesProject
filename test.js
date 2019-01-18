6394028c7c0f2d44e2956d591640037b1f77cea8
NieLamu <mxmxlty@gmail.com>
2019-01-18 10:42:23 +0800

    后台时间对齐

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
+const interval = 7
+
 const x = new Date()
 y = x.getMinutes()
 
 console.log(x);
-console.log(x);
+console.log(y);
+console.log(y%interval);
+const newMinutes = y-y%interval;
+console.log(newMinutes);
+x.setMinutes(newMinutes);
+console.log(x.valueOf());
+
 
