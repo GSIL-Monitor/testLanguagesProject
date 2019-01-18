e81fb28b8d0e7fb7ebed8b40e2be2cfe6af3c5fe
NieLamu <mxmxlty@gmail.com>
2019-01-18 20:05:12 +0800

    delete all codes in test.js

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
-const moment = require('moment');
-const sendingInterval = 5
 
-console.log(new Date())
-const now = moment();
-const oldSecond = now.second();
-console.log(oldSecond)
-const newSecond = oldSecond-oldSecond%sendingInterval
-console.log(newSecond)
-now.second(newSecond);
-console.log(now.unix())
-
-
-
-x = moment().milliseconds(0)
-console.log(x.unix());
