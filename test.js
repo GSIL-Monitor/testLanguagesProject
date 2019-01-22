afaa93187c8eb51c9325d7fdaf464806a7b17b2d
NieLamu <mxmxlty@gmail.com>
2019-01-22 17:38:54 +0800

    index in list is string

--- test.js
+++ test.js
@@ -1,37 +1,4 @@
-class drawEcgWithInterval {
-    constructor (){
-        this.timer = null;
-        this.x = 0;
-    }
-
-    startInterval (){
-        console.log('start', this.timer);
-        this.timer = setInterval(()=> {
-            this.x += 1;
-            console.log('x', this.x);
-        }, 500)
-        console.log('start', this.timer);
-    }
-
-    stopInterval (){
-        console.log('stop', this.timer);
-        this.timer && clearInterval(this.timer);
-        this.timer = null;
-        console.log('stop', this.timer);
-    }
-}
-
-
-// drawEcgWithInterval.startInterval()
-// drawEcgWithInterval.stopInterval()
-// setTimeout(drawEcgWithInterval.stopInterval, 2000);
-
-let drawEcg;
-function start() {
-    drawEcg = new drawEcgWithInterval();
-    drawEcg.startInterval();
-}
-
-function stop() {
-    drawEcg.stopInterval();
+const frameNum = [7, 3, 4];
+for (let i in frameNum){
+    console.log(i, typeof i)
 }
